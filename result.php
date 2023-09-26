c5697b5353e31db0fefba8ab5e3bb9cce78153b6
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-09-26 10:08:57 +0200

    fix: textarea

--- result.php
+++ result.php
@@ -1,35 +1,45 @@
 <?php
 // # DICHIARIAMO DUE VARIABILI CHE CONTENGONO I VALORI INVIATI DAL FORM
-  $userText = $_GET["userText"];
-  $censored = $_GET["censored"];
+$userText = $_GET["userText"];
+$censored = $_GET["censored"];
 
 // # TRAMITE IL REPLACE METTIAMO AL POSTO DELLA PAROLA CENSURATA TRE "*"
-  $result = str_replace($censored, "***", $userText); 
-  ?>
+$result = str_replace($censored, "***", $userText);
+?>
 
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Badwords</title>
-  </head>
-  
-  <body>
-    <!-- STAMPIAMO IL PARAGRAFO COSI' COM'é -->
-    <h2>Il tuo paragrafo è:</h2>
-    <p><?= $userText ?></p>
+<!DOCTYPE html>
+<html lang="en">
 
-    <!-- E LA SUA LUNGHEZZA -->
-    <h2>La sua lunghezza è:</h2>
-    <p><?= strlen($userText) ?></p>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Badwords</title>
+</head>
 
-    <!-- STAMPIAMO IL PARAGRAFO CENSURATO -->
-    <h2>Il tuo paragrafo censurato è:</h2>
-    <p><?php echo $result ?></p>
+<body>
+  <!-- STAMPIAMO IL PARAGRAFO COSI' COM'é -->
+  <h2>Il tuo paragrafo è:</h2>
+  <p>
+    <?= $userText ?>
+  </p>
 
-    <!-- E LA NUOVA LUNGHEZZA -->
-    <h2>La sua lunghezza ora è:</h2>
-    <p><?= strlen($result) ?></p> 
-  </body>
-  </html>
+  <!-- E LA SUA LUNGHEZZA -->
+  <h2>La sua lunghezza è:</h2>
+  <p>
+    <?= strlen($userText) ?>
+  </p>
+
+  <!-- STAMPIAMO IL PARAGRAFO CENSURATO -->
+  <h2>Il tuo paragrafo censurato è:</h2>
+  <p>
+    <?php echo $result ?>
+  </p>
+
+  <!-- E LA NUOVA LUNGHEZZA -->
+  <h2>La sua lunghezza ora è:</h2>
+  <p>
+    <?= strlen($result) ?>
+  </p>
+</body>
+
+</html>
